9d02fc475ab58d41f9594cc792129a6a4a605b80
Mohammed A. Alameedi <73557986+eng-alameedi@users.noreply.github.com>
2023-07-14 23:46:50 +0300

    Update

--- src/lib/veclib/vector2d.cpp
+++ src/lib/veclib/vector2d.cpp
@@ -2,40 +2,40 @@
 #include "vector2d.h"
 
 using namespace std;
-using namespace vector_engine;
-
-class vector2d
+namespace vector_engine
 {
-	private:
-		double i,j;
-	public:
-		vector2d::vector2d(double i = 0, double j = 0)
+		vector2d::vector2d()
+		{
+			this->i = 0;
+			this->j = 0;
+		}
+		vector2d::vector2d(double i, double j)
 		{
 			this->i = i;
 			this->j = j;
 		}
-		vector2d operator+(vector2d v)
+		vector2d vector2d::operator+(vector2d v)
 		{
 			vector2d c(0,0);
 			c.i = this->i + v.i;
 			c.j = this->j + v.j;
 			return c;
 		}
-		vector2d operator-(vector2d v)
+		vector2d vector2d::operator-(vector2d v)
 		{
 			vector2d c(0,0);
 			c.i = this->i - v.i;
 			c.j = this->j - v.j;
 			return c;
 		}
-		vector2d operator*(vector2d v)
+		vector2d vector2d::operator*(vector2d v)
 		{
 			vector2d c(0.0,0.0);
 			c.i = this->i * v.j;
 			c.j = this->j * v.i;
 			return c;
 		}
-		vector2d vector_input()
+		vector2d vector2d::vector_input()
 		{
 			vector2d c(0.0,0.0);
 			cout << "Please Enter Your Vector:" << endl;
@@ -51,7 +51,7 @@ class vector2d
 			}
 			return c;
 		}
-		double chk_number ()
+		double vector2d::chk_number ()
 		{
 			double number;
 			while(1)
@@ -71,15 +71,15 @@ class vector2d
 				}
 				return number;
 		}
-		void print(vector2d d)
+		void vector2d::print(vector2d d)
 		{
 			if (d.j > 0)
 				cout << "This Vector is: " << d.i <<"i+" << d.j <<"j" << endl;
 			else if (d.j < 0)
 				cout << "This Vector is: " << d.i <<"i" << d.j <<"j" << endl;
 		}
-		void multi_print(vector2d d)
+		void vector2d::multi_print(vector2d d)
 		{
 			cout <<"This Vector is: " << d.i-d.j <<"k" << endl;
 		}
-};
+}
--- src/lib/veclib/vector2d.h
+++ src/lib/veclib/vector2d.h
@@ -7,13 +7,14 @@ class vector2d
 	private:
 		double i,j;
 	public:
-		vector2d(double i = 0, double j = 0);
-		vector2d operator+(vector2d v);
-		vector2d operator-(vector2d v);
-		vector2d operator*(vector2d v);
+		vector2d();
+		vector2d(double, double);
+		vector2d operator+(vector2d);
+		vector2d operator-(vector2d);
+		vector2d operator*(vector2d);
 		vector2d vector_input();
 		double chk_number ();
-		void print(vector2d d);
-		void multi_print(vector2d d);
+		void print(vector2d);
+		void multi_print(vector2d);
 };
 }
