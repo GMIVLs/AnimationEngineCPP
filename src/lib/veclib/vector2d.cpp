8fffdd62dfe035a1334dbdf9b4e9cf12f7c82f55
Ghasak <mdotgh@gmail.com>
2023-07-16 23:29:33 +0900

    [sdl2] library is now ready to be utilized - using dynamic linking

--- src/lib/veclib/vector2d.cpp
+++ src/lib/veclib/vector2d.cpp
@@ -1,85 +1,84 @@
 #include <iostream>
+#include <limits>
 #include "vector2d.h"
 
 using namespace std;
 using namespace vector_engine;
 
-class vector2d
-{
-	private:
-		double i,j;
-	public:
-		vector2d::vector2d(double i = 0, double j = 0)
-		{
-			this->i = i;
-			this->j = j;
-		}
-		vector2d operator+(vector2d v)
-		{
-			vector2d c(0,0);
-			c.i = this->i + v.i;
-			c.j = this->j + v.j;
-			return c;
-		}
-		vector2d operator-(vector2d v)
-		{
-			vector2d c(0,0);
-			c.i = this->i - v.i;
-			c.j = this->j - v.j;
-			return c;
-		}
-		vector2d operator*(vector2d v)
-		{
-			vector2d c(0.0,0.0);
-			c.i = this->i * v.j;
-			c.j = this->j * v.i;
-			return c;
-		}
-		vector2d vector_input()
-		{
-			vector2d c(0.0,0.0);
-			cout << "Please Enter Your Vector:" << endl;
-			try
-			{
-				c.i = chk_number();
-				c.j = chk_number();
-			}
-			catch(...)
-			{
-				cout << "Error Accourd" << endl;
-				exit(3);
-			}
-			return c;
-		}
-		double chk_number ()
-		{
-			double number;
-			while(1)
-				{
-					cin >> number;
-					if(cin.fail())
-					{
-						cout << "Enter a Valid Number Please..." << endl;
-						cin.clear();
-						cin.ignore(numeric_limits<streamsize>::max(), '\n');
-						cin >> number;
-					}
-					if (!cin.fail())
-					{
-						break;
-					}
-				}
-				return number;
-		}
-		void print(vector2d d)
-		{
-			if (d.j > 0)
-				cout << "This Vector is: " << d.i <<"i+" << d.j <<"j" << endl;
-			else if (d.j < 0)
-				cout << "This Vector is: " << d.i <<"i" << d.j <<"j" << endl;
-		}
-		void multi_print(vector2d d)
-		{
-			cout <<"This Vector is: " << d.i-d.j <<"k" << endl;
-		}
-};
+vector2d::vector2d(double i, double j) {
+    this->i = i;
+    this->j = j;
+}
+
+vector2d vector2d::operator+(vector2d v) {
+    vector2d c(0,0);
+    c.i = this->i + v.i;
+    c.j = this->j + v.j;
+    return c;
+}
+
+vector2d vector2d::operator-(vector2d v) {
+    vector2d c(0,0);
+    c.i = this->i - v.i;
+    c.j = this->j - v.j;
+    return c;
+}
+
+vector2d vector2d::operator*(vector2d v) {
+    vector2d c(0.0,0.0);
+    c.i = this->i * v.j;
+    c.j = this->j * v.i;
+    return c;
+}
+
+vector2d vector2d::vector_input() {
+    vector2d c(0.0,0.0);
+    cout << "Please Enter Your Vector:" << endl;
+    try {
+        c.i = chk_number();
+        c.j = chk_number();
+    }
+    catch(...) {
+        cout << "Error Occurred" << endl;
+        exit(3);
+    }
+    return c;
+}
+
+double vector2d::chk_number () {
+    double number;
+    while(true) {
+        cin >> number;
+        if(cin.fail()) {
+            cout << "Enter a Valid Number Please..." << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cin >> number;
+        }
+        if (!cin.fail()) {
+            break;
+        }
+    }
+    return number;
+}
+
+/**
+ * @brief This function adds two integers.
+ *
+ This is a print function is working under the class vector2d
+ * Detailed description of the function.
+ *
+ * @param a The first integer.
+ * @param b The second integer.
+ * @return The sum of a and b.
+ */
+void vector2d::print(vector2d d) {
+  if (d.j > 0)
+    cout << "This Vector is: " << d.i << "i+" << d.j << "j" << endl;
+  else if (d.j < 0)
+    cout << "This Vector is: " << d.i << "i" << d.j << "j" << endl;
+}
+
+void vector2d::multi_print(vector2d d) {
+    cout <<"This Vector is: " << d.i-d.j <<"k" << endl;
+}
