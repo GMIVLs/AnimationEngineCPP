70d9f14089c5d830fb92109d07153313a5b477f9
Mohammed A. Alameedi <73557986+eng-alameedi@users.noreply.github.com>
2023-07-17 18:24:15 +0300

    update

--- src/lib/veclib/vector2d.cpp
+++ src/lib/veclib/vector2d.cpp
@@ -1,27 +1,81 @@
+//
+//  vector2d.cpp
+//  vector2d
+//
+//  Created by Mohammed A. Alameedi on 7/17/23.
+//
 
-using namespace std;
+#include "vector2d.h"
 
-
-namespace vector_engine
+template<class Ttype>
+vector2d<Ttype>::vector2d(Ttype x_coord, Ttype y_coord)
 {
-    vector2d::vector2d(V* x_coordinate = NULL,  V* y_coordinate = NULL)
-    {
-        this->x_coordinate = x_coordinate;
-        this->y_coordinate = y_coordinate;
-    }
-    vector2d::vector2d operator+(vector2d vector1)
-    {
-        vector2d* sum(0,0);
-        sum.x_coordinate = this->x_coordinate + vector1.x_coordinate;
-        sum.y_coordinate = this->y_coordinate + vector1.y_coordinate;
-        return sum;
-    }
-    vector2d::~vector2d()
-    {
-        delete (x_coordinate);
-        delete (y_coordinate);
-    }
+    this->x_coord = x_coord;
+    this->y_coord = y_coord;
 }
 
+template<class Ttype>
+vector2d<Ttype>::~vector2d()
+{
+    
+}
 
+template<class Ttype>
+void vector2d<Ttype>::set_x(Ttype x_coord)
+{
+    this->x_coord = x_coord;
+}
 
+template<class Ttype>
+void vector2d<Ttype>::set_y(Ttype y_coord)
+{
+    this->y_coord = y_coord;
+}
+
+template<class Ttype>
+Ttype vector2d<Ttype>::get_x() const
+{
+    return x_coord;
+}
+
+template<class Ttype>
+Ttype vector2d<Ttype>::get_y() const
+{
+    return y_coord;
+}
+
+template<class Ttype>
+vector2d<Ttype> vector2d<Ttype>::operator+(vector2d<Ttype> vect)
+{
+    return vector2d<Ttype>(this->x_coord+vect.x_coord,this->y_coord+vect.y_coord);
+}
+
+template<class Ttype>
+vector2d<Ttype> vector2d<Ttype>::operator-(vector2d<Ttype> vect)
+{
+    return vector2d<Ttype>(this->x_coord-vect.x_coord,this->y_coord-vect.y_coord);
+}
+
+template<class Ttype>
+vector2d<Ttype> vector2d<Ttype>::operator*(Ttype scaler)
+{
+    return vector2d<Ttype>(this->x_coord*scaler,this->y_coord*scaler);
+}
+
+template<class Ttype>
+vector2d<Ttype> vector2d<Ttype>::add(vector2d<Ttype> vect)
+{
+    return vector2d<Ttype>(this->x_coord+vect.x_coord,this->y_coord+vect.y_coord);
+}
+
+template<class Ttype>
+vector2d<Ttype> vector2d<Ttype>::sub(vector2d<Ttype> vect)
+{
+    return vector2d<Ttype>(this->x_coord-vect.x_coord,this->y_coord-vect.y_coord);
+}
+
+template<class Ttype>
+vector2d<Ttype> vector2d<Ttype>::multi(Ttype scaler)
+{
+    return vector2d<Ttype>(this->x_coord*scaler,this->y_coord*scaler);
+}
--- src/lib/veclib/vector2d.h
+++ src/lib/veclib/vector2d.h
@@ -1,19 +1,37 @@
-#pragma once
-namespace vector_engine {
-class vector2d {
-private:
-  double i, j;
+//
+//  vector2d.h
+//  vector2d
+//
+//  Created by Mohammed A. Alameedi on 7/17/23.
+//
 
+#ifndef vector2d_h
+#define vector2d_h
 
-namespace vector_engine
-{
-template<class V>
+template<class Ttype>
 class vector2d
 {
-    private:
-    V* x_coordinate, y_coordinate;
-    public:
-    vector2d(V*,V*);
-    vector2d operator+(V*);
+public:
+    // Class Vector Definetion
+    explicit vector2d(Ttype,Ttype);
+    ~vector2d();
+    //Vector set and get methods
+    void set_x(Ttype);
+    void set_y(Ttype);
+    Ttype get_x() const;
+    Ttype get_y() const;
+    //Vector operator overload
+    vector2d operator+(vector2d vector);
+    vector2d operator-(vector2d vector);
+    vector2d operator*(Ttype scaler);
+    //Vector methods
+    vector2d add(vector2d vector);
+    vector2d sub(vector2d vector);
+    vector2d multi(Ttype scaler);
+private:
+    //Definetion of Private Varibles of type Ttype
+    Ttype x_coord;
+    Ttype y_coord;
 };
-} // namespace vector_engine
+#endif /* vector2d_h */
+
